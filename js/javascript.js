c9495e9c52ff3e1370eff1b15c56d52b6dd0fe3e
Dimitar Kostov <dimitar.kostov@158ltd.com>
2016-05-09 18:21:27 +0300

    test

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,11 @@
-var image = document.querySelector('.team-image');
-
-image.addEventListener('mousemove', function(e) {
-  var x = e.clientX;
-  var y = e.clientY;
-  image.style['transform-origin'] = x + 'px ' + y + 'px';
+document.addEventListener('readystatechange', function() {
+  var image = document.querySelector('.team-image');
+  image.addEventListener('mousemove', function(e) {
+    requestAnimationFrame(function() {
+      var x = e.clientX;
+      var y = e.clientY;
+      console.log(image);
+      image.style['transform-origin'] = x + 'px ' + y + 'px';
+    });
+  });
 });
